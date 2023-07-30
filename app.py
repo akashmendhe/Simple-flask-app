940d5218497d47790c222b05f409a27a17612abf
akashm <mendheakash22@gmail.com>
2023-07-30 13:16:06 +0000

    Final code

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
-    # run() method of Flask class runs the application
-    # on the local development server.
-    app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
